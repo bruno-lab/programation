46b3dcbf169c15e5f27e017aa64439d7be489818
bruno-lab <brunoquadros9@gmail.com>
2020-12-14 23:08:13 -0300

    ajustes

--- functions.h
+++ functions.h
@@ -4,9 +4,17 @@ int calculaConvidados(int *pConvidados, int parada);
 
 float calculaPercentual(float parcela, float total);
 
-float calculaGarcon(float *custos);
+float calculaGarcon(float *custos, int *convidados);
 
-float calculaComida(float *custos);
+float calculaComida(float *custos, int *convidados);
 
-void compilaGastos(float orcamento);
+void compilaGastos(float orcamento, int *convidados);
+
+int comparaString(char primeiraString[30], char segundaString[30]);
+
+void gastosExtras();
+
+void ordenaAlfabetica(char (*conv)[30],int parada);
+
+void divideMesas();
 
